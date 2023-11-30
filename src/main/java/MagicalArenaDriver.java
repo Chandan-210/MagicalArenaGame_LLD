0f0ed49eebf7a0d618ab6a577198961a3f5b4f8e
Chandan Chadha <chandan.chadha210@gmail.com>
2023-11-30 14:58:28 +0530

    Commit 3: added roll() method inside Dice class to get random number after each dice roll.

--- src/main/java/MagicalArenaDriver.java
+++ src/main/java/MagicalArenaDriver.java
@@ -1,2 +1,6 @@
+import java.util.Scanner;
 public class MagicalArenaDriver {
+    public static void main(String args[]){
+
+    }
 }
--- src/main/java/dice/Dice.java
+++ src/main/java/dice/Dice.java
@@ -1,4 +1,12 @@
 package dice;
-
+import java.util.Random;
 public class Dice {
+    private Random random;
+    public Dice(){
+        this.random = new Random();
+    }
+    public int roll(){
+        //since the dice is a 6-sided object
+        return random.nextInt(6) + 1;
+    }
 }
