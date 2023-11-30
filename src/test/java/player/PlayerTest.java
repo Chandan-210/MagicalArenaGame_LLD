751b277c6557f7fc67aeda71ca9564cb376b76e6
Chandan Chadha <chandan.chadha210@gmail.com>
2023-11-30 16:23:31 +0530

    Commit 14: added more custom test cases for PlayerTest.java to initialize the player with 1. Negative Values 2. Null (Zero) values 3. Large values

--- src/test/java/player/PlayerTest.java
+++ src/test/java/player/PlayerTest.java
@@ -6,10 +6,29 @@ public class PlayerTest {
     @Test
     public void testPlayerCreation() {
         //use the same input method as present in the Main (MagicalArena Driver) function
+        // Test case 1: create a Valid Player
         Player player = createPlayerFromInput("50 5 10");
         assertEquals(50, player.getHealth());
         assertEquals(5, player.getStrength());
         assertEquals(10, player.getAttack());
+
+        // Test case 2: create player with zero values for health, strength, and attack
+        player = createPlayerFromInput("0 0 0");
+        assertEquals(0, player.getHealth());
+        assertEquals(0, player.getStrength());
+        assertEquals(0, player.getAttack());
+
+        // Test case 3: create player with negative values for health, strength, and attack
+        player = createPlayerFromInput("-10 -5 -3");
+        assertEquals(-10, player.getHealth());
+        assertEquals(-5, player.getStrength());
+        assertEquals(-3, player.getAttack());
+
+        // Test case 4: create player with large values for health, strength, and attack
+        player = createPlayerFromInput("1000000 50000 10000");
+        assertEquals(1000000, player.getHealth());
+        assertEquals(50000, player.getStrength());
+        assertEquals(10000, player.getAttack());
     }
     // helper method to create a Player instance from user-provided input
     private Player createPlayerFromInput(String input) {
