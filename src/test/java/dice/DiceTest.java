05b8df44d1e7e2304973b7a8e61ce13a11a849b9
Chandan Chadha <chandan.chadha210@gmail.com>
2023-11-30 16:26:59 +0530

    Commit 15: added more custom test cases for DiceTest.java to roll the dice 1. Only once 2. Multiple times

--- src/test/java/dice/DiceTest.java
+++ src/test/java/dice/DiceTest.java
@@ -6,8 +6,15 @@ public class DiceTest {
     public void testDiceRoll(){
         // Use the same input method as in the main function
         Dice dice = new Dice();
-        int rollResult = dice.roll();
-
+        int rollResult;
+        //Test case 1: roll the dice 1 time
+        rollResult = dice.roll();
         assertTrue(rollResult >= 1 && rollResult <= 6);
+
+        //Test case 2: roll the dice multiple times eg. 1000
+        for (int i = 0; i < 1000; i++) {
+            rollResult = dice.roll();
+            assertTrue("Dice Roll result should be between 1 and 6", rollResult >= 1 && rollResult <= 6);
+        }
     }
 }
