639fc542d164dabdab50c0363c92a4f4cd363d75
Chandan Chadha <chandan.chadha210@gmail.com>
2023-11-30 16:14:09 +0530

    Commit 12: created JUnit tests for PlayerTest.java in order to initialize a player with its attributes and check for results. 1 custom user test case has been provided for prototype run

--- src/test/java/player/PlayerTest.java
+++ src/test/java/player/PlayerTest.java
@@ -1,4 +1,14 @@
 package player;
-
+import org.junit.Test;
+import java.util.Scanner;
+import static org.junit.Assert.assertEquals;
 public class PlayerTest {
+    @Test
+    public void testPlayerCreation() {
+        //use the same input method as present in the Main (MagicalArena Driver) function
+        Player player = createPlayerFromInput("50 5 10");
+        assertEquals(50, player.getHealth());
+        assertEquals(5, player.getStrength());
+        assertEquals(10, player.getAttack());
+    }
 }
