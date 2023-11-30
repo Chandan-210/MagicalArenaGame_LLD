8d29781415b391484d0b327af0454e70c463e4db
Chandan Chadha <chandan.chadha210@gmail.com>
2023-11-30 15:39:06 +0530

    Commit 8: updated game logic for MagicalArena.java under method startMatch() of class ArenaSimulationResult.java to 1. allow both players to take turns. 2. calculate damage dealt after attack + damage received after defense. 3. update the attributes of both players after each turn i.e. Health, Attack, Strength 4. return true informing the user that the game has concluded when health of a player = 0.

--- src/main/java/arena/MagicalArena.java
+++ src/main/java/arena/MagicalArena.java
@@ -1,5 +1,6 @@
 package arena;
 import player.Player;
+import dice.Dice;
 import java.util.Random;
 public class MagicalArena {
     private final Player playerA;
@@ -10,6 +11,48 @@ public class MagicalArena {
         this.playerA = playerA;
         this.playerB = playerB;
         this.random = new Random();
+    }
+
+    public ArenaSimulationResult startMatch() {
+        Dice dice = new Dice();
+        while (playerA.getHealth() > 0 && playerB.getHealth() > 0) {
+            // Player A's turn
+            int attackRollA = dice.roll();
+            int damageA = playerA.getAttack() * attackRollA;
+
+            // Player B's defense
+            int defenseRollB = dice.roll();
+            int defenseB = playerB.getStrength() * defenseRollB;
+
+            // Calculate damage dealt by A after defense
+            int actualDamageA = Math.max(0, damageA - defenseB);
+            playerB.reduceHealth(actualDamageA);
+
+            // Check if Player B is defeated
+            if (playerB.getHealth() == 0) {
+                return new ArenaSimulationResult(false);
+            }
+
+            // Player B's turn
+            int attackRollB = dice.roll();
+            int damageB = playerB.getAttack() * attackRollB;
+
+            // Player A's defense
+            int defenseRollA = dice.roll();
+            int defenseA = playerA.getStrength() * defenseRollA;
+
+            // Calculate damage dealt by B after defense
+            int actualDamageB = Math.max(0, damageB - defenseA);
+            playerA.reduceHealth(actualDamageB);
+
+            // Check if Player A is defeated
+            if (playerA.getHealth() == 0) {
+                return new ArenaSimulationResult(false);
+            }
+        }
+
+        // The match is concluded
+        return new ArenaSimulationResult(true);
     }
 
 }
