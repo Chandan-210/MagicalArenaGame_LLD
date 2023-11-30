40aa644d7388054e5ebd22e81621e6f8a9aea5fd
Chandan Chadha <chandan.chadha210@gmail.com>
2023-11-30 16:41:38 +0530

    Commit 17: created Scanner class object input stream to allow user to create user defined input for each player and get attributes.

--- src/main/java/MagicalArenaDriver.java
+++ src/main/java/MagicalArenaDriver.java
@@ -1,6 +1,15 @@
+import player.Player;
+import arena.MagicalArena;
+import arena.ArenaSimulationResult;
 import java.util.Scanner;
 public class MagicalArenaDriver {
-    public static void main(String args[]){
+    public static void main(String[] args){
+        //enter Scanner object input stream
+        Scanner scanner  = new Scanner(System.in);
 
+        //Enter input/ details for PLayer A
+        System.out.println("Welcome: Player A");
+        System.out.println("Enter your attributes");
+        Player playerA = createPlayer()
     }
 }
--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,4 +1,5 @@
 package player;
+import java.util.Scanner;
 
 public class Player {
     private int health;
@@ -34,5 +35,22 @@ public class Player {
         if(health < 0){
             health = 0;
         }
+    }
+    // Method to create a Player instance with user-defined input
+    public static Player createPlayer() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter health: ");
+        int health = scanner.nextInt();
+
+        System.out.print("Enter strength: ");
+        int strength = scanner.nextInt();
+
+        System.out.print("Enter attack: ");
+        int attack = scanner.nextInt();
+
+        scanner.close();
+
+        return new Player(health, strength, attack);
     }
 }
