cb75b2f4ba5d028dcb32d09ce6921148856c3025
Chandan Chadha <chandan.chadha210@gmail.com>
2023-11-30 16:06:23 +0530

    Commit 10: created JUnit tests for MagicalArena scenario in order to start match and check for results. 1 custom user test case has been provided for prototype run.

--- src/test/java/arena/MagicalArenaTest.java
+++ src/test/java/arena/MagicalArenaTest.java
@@ -1,4 +1,18 @@
 package arena;
-
+import org.junit.Test;
+import player.Player;
+import java.util.Scanner;
+import static org.junit.Assert.assertFalse;
 public class MagicalArenaTest {
+    @Test
+    public void testMatchResult() {
+        // Use the same input method as in the main function
+        Player playerA = createPlayerFromInput("50 5 10");
+        Player playerB = createPlayerFromInput("100 10 5");
+
+        MagicalArena arena = new MagicalArena(playerA, playerB);
+        ArenaSimulationResult result = arena.startMatch();
+
+        assertFalse(result.isMatchInProgress());
+    }
 }
