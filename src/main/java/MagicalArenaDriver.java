eba43c7709718331b1e29726e22206c8de914457
Chandan Chadha <chandan.chadha210@gmail.com>
2023-11-30 16:44:20 +0530

    Commit 18: initialized the createPlayer() method from the player package to provide user defined input for each player.

--- src/main/java/MagicalArenaDriver.java
+++ src/main/java/MagicalArenaDriver.java
@@ -1,15 +1,22 @@
 import player.Player;
 import arena.MagicalArena;
 import arena.ArenaSimulationResult;
-import java.util.Scanner;
+//import java.util.Scanner;
 public class MagicalArenaDriver {
     public static void main(String[] args){
         //enter Scanner object input stream
-        Scanner scanner  = new Scanner(System.in);
+        //Scanner scanner  = new Scanner(System.in);
 
         //Enter input/ details for PLayer A
         System.out.println("Welcome: Player A");
         System.out.println("Enter your attributes");
-        Player playerA = createPlayer()
+        Player playerA = player.Player.createPlayer();
+
+        //Enter input/ details for PLayer B
+        System.out.println("Welcome: Player B");
+        System.out.println("Enter your attributes");
+        Player playerB = player.Player.createPlayer();
+
+
     }
 }
