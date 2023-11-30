c963b6d92c305ac9010ff197b9ec95f09bd03e6d
Chandan Chadha <chandan.chadha210@gmail.com>
2023-11-30 15:19:22 +0530

    Commit 5: changed classes under player package. Added Player attributes to Player class, and called all the getter and setter methods.

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,4 +1,28 @@
 package player;
 
 public class Player {
+    private int health;
+    private int strength;
+    private int attack;
+
+    public Player(int health, int strength, int attack){
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+    public int getHealth(){
+        return health;
+    }
+    public int getStrength(){
+        return strength;
+    }
+    public int getAttack(){
+        return attack;
+    }
+    public void reduceHealth(int damage){
+        health -= damage;
+        if(health < 0){
+            health = 0;
+        }
+    }
 }
