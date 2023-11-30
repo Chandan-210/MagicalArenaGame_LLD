fc1092b4ec090ec293d36aaf6363c006aa8a5d65
Chandan Chadha <chandan.chadha210@gmail.com>
2023-11-30 15:32:26 +0530

    Commit 7: entered player details and random class object to start the game from the Arena.

--- src/main/java/arena/MagicalArena.java
+++ src/main/java/arena/MagicalArena.java
@@ -1,4 +1,15 @@
 package arena;
-
+import player.Player;
+import java.util.Random;
 public class MagicalArena {
+    private final Player playerA;
+    private final Player playerB;
+    private final Random random;
+
+    public MagicalArena(Player playerA, Player playerB){
+        this.playerA = playerA;
+        this.playerB = playerB;
+        this.random = new Random();
+    }
+
 }
