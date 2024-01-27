e486da12eeddc7c7b18a550660ef71cc6591402a
Chandan Chadha <chandan.chadha210@gmail.com>
2024-01-27 16:52:42 +0530

    Commit 28: committed for "DiceTest.java": 1. created test cases to test multiple dice rolls and for dice randomness. 2. Method names are as follows for the test cases: 2.1 for validating multiple dice rolls: testDiceRollMultiple() 2.2 for ensuring randomness of consecutive dice rolls testDiceRollRandomness()

--- src/test/java/dice/DiceTest.java
+++ src/test/java/dice/DiceTest.java
@@ -4,19 +4,29 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNotEquals;
 public class DiceTest {
     @Test
-    public void testDiceRoll(){
+    public void testDiceRoll() {
         // Use the same input method as in the main function
         Dice dice = new Dice();
         int rollResult;
         //Test case 1: roll the dice 1 time
         rollResult = dice.roll();
         assertTrue(rollResult >= 1 && rollResult <= 6);
+    }
+    @Test
+    public void testDiceRollMultiple() {
+        Dice dice = new Dice();
+        int rollResult;
 
         //Test case 2: roll the dice multiple times eg. 1000
         for (int i = 0; i < 1000; i++) {
             rollResult = dice.roll();
             assertTrue("Dice Roll result should be between 1 and 6", rollResult >= 1 && rollResult <= 6);
         }
+    }
+    @Test
+    public void testDiceRollRandomness(){
+        Dice dice = new Dice();
+        int rollResult;
 
         //Test case 3: tertiary test case to check if 2 consecutive dice rolls produce the same result
         rollResult = dice.roll();
